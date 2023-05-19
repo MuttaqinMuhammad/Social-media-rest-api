5a0da99035aade0c77963e2bc3b0727b0de43dee
muttaqin1 <mdmuttaqin20@gmail.com>
2023-05-19 19:41:08 +0600

    Remove Notification and story feature.

--- routes/notificationRoute.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router()
-const { showNotifications, viewSource } = require('../controllers/notification')
-const auth = require('../middlewares/common/auth')
-
-router.get('/', auth, showNotifications)
-router.get('/:notificationId', auth, viewSource)
-
-module.exports = router
