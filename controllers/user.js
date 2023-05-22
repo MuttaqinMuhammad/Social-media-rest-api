4fea66bae445a2e8d4fc48c3aaf9b20c47592b86
muttaqin1 <mdmuttaqin20@gmail.com>
2023-05-22 13:14:33 +0600

    Refactor user controller.

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,5 @@
 const User = require('../models/User');
 const Profile = require('../models/Profile');
-const Story = require('../models/Storie');
 const OTP = require('../models/OTP');
 const config = require('config');
 const bcrypt = require('bcryptjs');
@@ -13,55 +12,54 @@ const sendMail = require('../helpers/sendMail');
 const otpGenerator = require('../helpers/user/otpGenerator');
 const OTP = require('../models/OTP');
 const User = require('../models/User');
+const {
+  BadRequestError,
+  NotFoundError,
+  UnauthorizationError,
+} = require('../helpers/ApiError');
 
 const sendOtp = async (req, res, next) => {
   const { email } = req.body;
-  const user = await User.findOne({ email });
+  try {
+    const user = await User.findOne({ email });
 
-  if (user) {
-    await OTP.deleteOne({ user: user._id });
+    if (user) throw new NotFoundError('No user found with the email.');
+    //delete if any otp exists.
+    await OTP.deleteMany({ user: user._id });
     const randomOTP = otpGenerator(6);
-    const otp = new OTP({
+    await OTP.create({
       user: user._id,
       otp: randomOTP,
     });
-    try {
-      await otp.save();
-      const mailParam = {
-        title: `your ${APP_NAME} OTP `,
-        body: `Here is your OTP:${randomOTP}. please dont share this to anyone . this token will be expired in 10 munites.`,
-        emailReciever: email,
-      };
-      const sendMailToUser = sendMail(mailParam);
-      const userData = {
-        userId: user._id,
-        name: user.name,
-        email: user.email,
-        avatar: user.avatar,
-      };
-      const payload = jwt.sign(userData, JWT_SECRET_KEY, {
-        expiresIn: 1000 * 60 * 10,
-      });
-      //redirect validate otp
-      res
-        .status(200)
-        .cookie('validate-otp', payload, {
-          httpOnly: true,
-          signed: true,
-          maxAge: 1000 * 60 * 10,
-        })
-        .json({
-          success: true,
-          message: 'an otp has been sended to your email',
-        });
-    } catch (e) {
-      next(e);
-    }
-  } else {
-    res.status(500).json({
-      success: false,
-      message: 'no user found',
+    const mailParam = {
+      title: `your ${APP_NAME} OTP `,
+      body: `Here is your OTP:${randomOTP}. please dont share this to anyone . this token will be expired in 10 munites.`,
+      emailReciever: email,
+    };
+    sendMail(mailParam);
+    const userData = {
+      userId: user._id,
+      name: user.name,
+      email: user.email,
+      avatar: user.avatar,
+    };
+    const payload = jwt.sign(userData, JWT_SECRET_KEY, {
+      expiresIn: 1000 * 60 * 10,
     });
+    //redirect validate otp
+    res
+      .status(204)
+      .cookie('validate-otp', payload, {
+        httpOnly: true,
+        signed: true,
+        maxAge: 1000 * 60 * 10,
+      })
+      .json({
+        success: true,
+        message: 'an otp has been sended to your email',
+      });
+  } catch (e) {
+    next(e);
   }
 };
 
@@ -76,7 +74,7 @@ const validateOtp = async (req, res, next) => {
   try {
     if (cookie && otp) {
       const decoded = jwt.verify(cookie, JWT_SECRET_KEY);
-      const { userId, name, avatar, email } = decoded;
+      const { userId } = decoded;
       const dataOtp = await OTP.findOne({ user: userId });
 
       if (dataOtp.otp === otp) {
@@ -90,7 +88,7 @@ const validateOtp = async (req, res, next) => {
         );
 
         //redirect create a new password route
-        return res.status(200).json({
+        return res.status(204).json({
           success: true,
         });
       } else {
@@ -106,20 +104,23 @@ const validateOtp = async (req, res, next) => {
 
 const signup = async (req, res, next) => {
   const { name, email, password, birthday, gender } = req.body;
+  // check if a user is already associated with the email.
+  const exists = await User.findOne({ email });
+  if (exists) throw new BadRequestError('User is already registered.');
+  // hash the password.
   const hashedPassword = await bcrypt.hash(password, 10);
-  const user = new User({
-    name,
-    email,
-    password: hashedPassword,
-    birthday,
-    gender,
-  });
-
   try {
-    const newUser = await user.save();
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+      birthday,
+      gender,
+    });
+    user.password = null;
     res.status(200).json({
       success: true,
-      newUser,
+      user,
     });
   } catch (e) {
     next(e);
@@ -163,44 +164,42 @@ const GoogleCallback = async (req, res, next) => {
 const login = async (req, res, next) => {
   try {
     const { email, password } = req.body;
-
+    // find the user.
     const user = await User.findOne({
       email,
     }).select('+password');
-    if (user._id) {
-      const verify = await bcrypt.compare(password, user.password);
-      if (verify) {
-        const token = jwt.sign(
-          {
-            userId: user._id,
-          },
-          JWT_SECRET_KEY,
-          {
-            expiresIn: JWT_EXPIRY_TIME,
-          }
-        );
-
-        res.cookie(AUTH_COOKIE_NAME, token, {
-          httpOnly: true,
-          signed: true,
-          maxAge: 86400000,
-        });
-        res.status(200).json({
-          success: true,
-          user,
-          token,
-        });
-      } else {
-        throw new Error('Access denied!');
+    if (!user._id) throw new UnauthorizationError('User is not registered.');
+    // verify the password
+    const verify = await bcrypt.compare(password, user.password);
+    if (verify) throw new UnauthorizationError('Invalid password.');
+    // generate a jwt token.
+    const token = jwt.sign(
+      {
+        userId: user._id,
+      },
+      JWT_SECRET_KEY,
+      {
+        expiresIn: JWT_EXPIRY_TIME,
       }
-    }
+    );
+    user.password = null;
+
+    res.cookie(AUTH_COOKIE_NAME, token, {
+      httpOnly: true,
+      signed: true,
+      maxAge: 86400000,
+    });
+    res.status(200).json({
+      success: true,
+      user,
+      token,
+    });
   } catch (e) {
-    e.message = 'Access denied!';
     next(e);
   }
 };
 
-const logout = async (req, res, next) => {
+const logout = async (req, res) => {
   res.clearCookie(AUTH_COOKIE_NAME);
   res.status(200).json({
     success: true,
@@ -210,33 +209,30 @@ const logout = async (req, res, next) => {
 
 const changePasswordWithOtp = async (req, res, next) => {
   const { password } = req.body;
-
+  //get all the cookies
   const cookies =
     Object.keys(req.signedCookies).length > 0 ? req.signedCookies : false;
+  // find the validate otp cookie
   const cookie = cookies['validate-otp'] ? cookies['validate-otp'] : false;
   try {
-    if (cookie) {
-      const { userId } = jwt.verify(cookie, JWT_SECRET_KEY);
-      const otp = await OTP.findOne({ user: userId });
-      console.log(otp);
-      if (otp.isValid) {
-        await User.updateOne(
-          { _id: userId },
-          {
-            $set: {
-              password: hashedPassword(password),
-            },
-          }
-        );
-        res.status(200).clearCookie('validate-otp').json({
-          success: true,
-        });
-      } else {
-        throw new Error('authentication failure');
+    //check if the cookie exists.
+    if (cookie) throw new UnauthorizationError();
+    // verify the token.
+    const { userId } = jwt.verify(cookie, JWT_SECRET_KEY);
+    // find the otp.
+    const otp = await OTP.findOne({ user: userId });
+    if (otp.isValid) throw new UnauthorizationError();
+    await User.updateOne(
+      { _id: userId },
+      {
+        $set: {
+          password: hashedPassword(password),
+        },
       }
-    } else {
-      throw new Error('authentication failure');
-    }
+    );
+    res.status(200).clearCookie('validate-otp').json({
+      success: true,
+    });
   } catch (e) {
     next(e);
   }
@@ -244,18 +240,11 @@ const changePasswordWithOtp = async (req, res, next) => {
 
 const deleteAccount = async (req, res, next) => {
   try {
-    const story = await Story.find({ creator: req.user._id });
-    if (Story.length > 0) {
-      story.forEach(async (storyObject) => {
-        await Story.deleteOne({ _id: storyObject._id });
-      });
-    }
     await deletePosts(req.user._id);
     await Profile.deleteOne({ user: req.user._id });
     await User.deleteOne({ _id: req.user._id });
 
     //unfriend all friends.
-    // remove all the pending friend requests which user sended others.
     //remove all friend requests
     res.clearCookie(AUTH_COOKIE_NAME).json({
       success: true,
